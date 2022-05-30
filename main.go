8103b4f5b65ab65b6b3290d9e72986e926b72d6a
Faruk Musa <musadeola@gmail.com>
2022-05-30 14:25:00 -0400

    recipe data model

--- main.go
+++ main.go
@@ -1,7 +1,15 @@
 package main
+
 import "github.com/gin-gonic/gin"
+
 func main() {
-    router := gin.Default()
-    router.Run()
-          
+	router := gin.Default()
+	router.Run()
+}
+type Recipe struct {
+	Name         string    'json:"name"'
+	Tags         []string  'json:"tags"'
+	Ingredients  []string  'json:"ingredients"'
+	Instructions []string  'json:"instructions"'
+	PublishedAt  time.Time 'json:"publishedAt"'
 }
